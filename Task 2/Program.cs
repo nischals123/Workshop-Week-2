ad2fc1c10b71e7f64f34e68170f6bb5e2198f5e8
Nischal <nischals054@gmail.com>
2025-11-04 12:19:32 +0545

    task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Circle
+{
+    public const double PI = 3.14;
+
+    public static double Area(double radius)
+    {
+        return PI * radius * radius;
+    }
+    public static double Perimeter(double radius)
+    {
+        return 2 * PI * radius;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Value of PI: {Circle.PI}");
+
+        double radius = 5;
+        Console.WriteLine($"Area of circle: {Circle.Area(radius)}");
+        Console.WriteLine($"Perimeter of circle: {Circle.Perimeter(radius)}");
+    }
+}
