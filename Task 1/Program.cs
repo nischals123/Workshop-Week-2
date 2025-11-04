a8270553ec5b761c2f38f8e345de0f6f77004815
Nischal <nischals054@gmail.com>
2025-11-04 11:52:02 +0545

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string userName = "Nischal";
+int luckyNumber = 1;
+
+Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
