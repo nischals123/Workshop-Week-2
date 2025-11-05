ec17c8a8ba1a0e42a8598593afea966aa1b8e2dd
Nischal <nischals054@gmail.com>
2025-11-05 06:59:23 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] favNumbers = { 8, 3, 15, 1, 10 };
+
+Array.Sort(favNumbers);
+Array.Reverse(favNumbers);
+
+Console.WriteLine(" Favorite Numbers ");
+for (int i = 0; i < favNumbers.Length; i++)
+{
+    Console.WriteLine($"Element {i}: {favNumbers[i]}");
+}
+
+int position = Array.IndexOf(favNumbers, 10);
+Console.WriteLine($"\nThe position of number 10 is: {position}");
